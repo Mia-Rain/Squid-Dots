8c073789401f0bdd782f5e96796e107f59f241f4
ThatGeekyWeeb <ssfgames13@gmail.com>
2020-08-05 16:56:31 -0400

    Plugin can now be disabled!

--- Discord-Plugins/Anti-Parency.plugin.js
+++ Discord-Plugins/Anti-Parency.plugin.js
@@ -14,7 +14,7 @@ NoWall.prototype.addToolbarItem = function(){
                     newToolbarItem.style.justifyContent = 'flex-start'
                     newToolbarItem.style.cursor = 'pointer'
                     newToolbarItem.style.paddingTop = '25px'
-                    newToolbarItem.innerHTML = '<span class="show-channels-button toolbar-1t6TWx active" style="width: 26px; position: relative; top: -2px; display: flex; flex-direction: column; justify-content: flex-start; cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx" style="width: 26px;position: relative;top: -6px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;font-family: serif !important;"><span class="show-channels-button toolbar-1t6TWx active" style="position: relative;top: -2px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;padding-right: 30px;padding-bottom: 25px;"><span class="show-channels-button toolbar-1t6TWx active" style="position: initial;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx active" style="position: absolute;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;margin-bottom: 15px;"><span style="display: block;color: var(--text-normal);-webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 1px;text-align: center;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(5.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 19px;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;padding-bottom: 5px;">-</span>';
+                    newToolbarItem.innerHTML = '<span id="NoWall" class="show-channels-button toolbar-1t6TWx active" style="width: 26px; position: relative; top: -2px; display: flex; flex-direction: column; justify-content: flex-start; cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx" style="width: 26px;position: relative;top: -6px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;font-family: serif !important;"><span class="show-channels-button toolbar-1t6TWx active" style="position: relative;top: -2px;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;padding-right: 30px;padding-bottom: 25px;"><span class="show-channels-button toolbar-1t6TWx active" style="position: initial;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;"><span class="show-channels-button toolbar-1t6TWx active" style="position: absolute;display: flex;flex-direction: column;justify-content: flex-start;cursor: pointer;margin-bottom: 15px;"><span style="display: block;color: var(--text-normal);-webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 1px;text-align: center;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(5.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;height: 19px;">-</span><span style="display: block; color: var(--text-normal); -webkit-transform: scale(2.0, 1.0);-moz-transform:scale(2.0, 1.0);-ms-transform:scale(2.0, 1.0);-o-transform:scale(2.0, 1.0);transform: scale(5,2);width: 0;padding-bottom: 5px;">-</span>';
 
         document.querySelector('.inner-MADQqc').insertBefore(newToolbarItem,document.querySelector('[class*="attachWrapper-"]'));
 
@@ -37,7 +37,7 @@ NoWall.prototype.getDescription = function(){
     return "Create a button that removes the wallpaper";
 };
 NoWall.prototype.getVersion = function(){
-    return "1.0.0";
+    return "1.1.0";
 };
 NoWall.prototype.getAuthor = function(){
     return "ThatGeekyWeeb";
@@ -47,12 +47,10 @@ NoWall.prototype.start = function(){
     console.log("NoWall loaded");
 };
 NoWall.prototype.stop = function(){
-    if(document.querySelectorAll('.show-channels-button').length) {
-        document.querySelector('.show-channels-button').outerHTML = "";
-        delete document.querySelector('.show-channels-button');
-    }
-    document.querySelector('.appMount-3lHmkl').style.background = ''
-    console.log("NoWall unloaded");
+	        var list = document.getElementById("NoWall");
+	        list.removeChild(list.childNodes[0]);
+        document.querySelector('.appMount-3lHmkl').style.background = '';
+        console.log("NoWall unloaded");
 };
 
 NoWall.prototype.onSwitch = function(){
